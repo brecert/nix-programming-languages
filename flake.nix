a1ba3e309020a28a6c113acd228dd1c0aa4604c5
bree <me@bree.dev>
2023-01-30 20:30:48 -0500

    init: add initial flake and dyon

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { flake-parts, ... }@inputs: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = ["x86_64-linux"];
+
+      perSystem = { pkgs, ... }: {
+        packages = {
+          dyon = pkgs.callPackage ./languages/dyon { };
+        };
+      };
+    };
+}
